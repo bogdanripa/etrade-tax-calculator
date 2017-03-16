5ac83792d3ce1838525071361dfec0dd4cd6d887
Mihai Iancu <miancu@adobe.com>
2017-03-16 12:46:44 +0200

    tab to spaces ... duuuuh

--- calculate.js
+++ calculate.js
@@ -14,72 +14,72 @@ var updateDates2 = function() {
     var scope = angular.element($('.ordersGrantTbl')).scope()
     var settled = scope.oh.openOrder.filter(function(e){return e.orderStatus == "Settled"})
 
-	var totalProfits = 0;
+    var totalProfits = 0;
 
-	// for each row
-	for (var i=0;i<settled.length; i++) {
+    // for each row
+    for (var i=0;i<settled.length; i++) {
         var order = settled[i]
-		var lineComission = 0;
-		var stockPrice = 0;
+        var lineComission = 0;
+        var stockPrice = 0;
 
-		// get the selling proce per share for the table
-		var sellingPricePerShare = order.executedPrice
+        // get the selling proce per share for the table
+        var sellingPricePerShare = order.executedPrice
 
-		var orderGrossRevenue = 0;
-		var orderCost = 0;
+        var orderGrossRevenue = 0;
+        var orderCost = 0;
 
-		// calculate the line-level lineComission
-		lineComission = order.disbursementDetails[0].commission+
+        // calculate the line-level lineComission
+        lineComission = order.disbursementDetails[0].commission+
                         order.disbursementDetails[0].disbursementFee+
                         order.disbursementDetails[0].brokerageAssistFee+
                         order.disbursementDetails[0].secFee;
 
         console.info("Order: ",order.soldQty,"*",order.executedPrice)
 
-		// for all grants in this transaction
-		for (var grant of order.purchaseGrantDetails) {
+        // for all grants in this transaction
+        for (var grant of order.purchaseGrantDetails) {
 
-			var sharesCount = grant.sharesSellSold
+            var sharesCount = grant.sharesSellSold
 
-			orderGrossRevenue += sellingPricePerShare * sharesCount;
+            orderGrossRevenue += sellingPricePerShare * sharesCount;
 
             var RSU = (grant.type == "RSU")
-			var date = grant.vestDate;
+            var date = grant.vestDate;
             if (!RSU){
                 date = grant.purchaseDate;
             }
-			var dateObj = new Date(date);
+            var dateObj = new Date(date);
 
             var stockPrice = 0;
 
-			if (RSU || (!RSU && (dateObj <= new Date('12/31/2011') || dateObj >= new Date('12/31/2013')))) {
-				// if RSU or ESPP, but before 2012 or after 2013
-				// use the market value
+            if (RSU || (!RSU && (dateObj <= new Date('12/31/2011') || dateObj >= new Date('12/31/2013')))) {
+                // if RSU or ESPP, but before 2012 or after 2013
+                // use the market value
                 
                 while(stockPrice == 0){
-				    stockPrice = adobeStockValues[date] || 0;
+                    stockPrice = adobeStockValues[date] || 0;
                     // console.debug("\t\t go back 1 day and get close price", date, stockPrice)
                     // go back 1 day nasdaq was closed  
                     var dateMinus1 = new Date(dateObj.getTime()-24*60*60*1000)
                     var d=dateMinus1.toISOString().split("T")[0].split('-')
                     date = d[1]+"/"+d[2]+"/"+d[0]
                 }
-			} else {
-				// if ESPP between and taxes were not paid on the diff
-				// use the grant purchase value
-				stockPrice = grant.purchasePrice;
-			}
+            } else {
+                // if ESPP between and taxes were not paid on the diff
+                // use the grant purchase value
+                stockPrice = grant.purchasePrice;
+            }
 
-			var grantCost = stockPrice * sharesCount;
+            var grantCost = stockPrice * sharesCount;
             orderCost += grantCost
             console.info("\t",RSU?"[RSU ]":"[ESPP]","grant cost: ",grantCost,"=",stockPrice,"*",sharesCount)
-		}
+        }
         var orderProfit = orderGrossRevenue-orderCost-lineComission
         console.info("order profit: ",orderProfit," = ",orderGrossRevenue,"-",orderCost,"-",lineComission )
-		totalProfits += orderProfit
-	}
+        totalProfits += orderProfit
+    }
 
-	totalProfits = parseInt(totalProfits*100)/100;
+    totalProfits = parseInt(totalProfits*100)/100;
     console.info("total profit ", totalProfits)
 
 }
